c775472be45c17db49283bb2f613b17c43e68843
Xu Haiming <haiming12322@qq.com>
2018-04-04 23:27:32 +0800

    Remove space for CDATA

--- app/helpers/buildResponseTemplate.js
+++ app/helpers/buildResponseTemplate.js
@@ -1,5 +1,5 @@
 const text = ({ toUser, fromUser, content }) => (
-  `<xml> <ToUserName>< ![CDATA[${toUser}] ]></ToUserName> <FromUserName>< ![CDATA[${fromUser}] ]></FromUserName> <CreateTime>1522855306</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA[${content}] ]></Content> </xml>`);
+  `<xml><ToUserName><![CDATA[${toUser}]]></ToUserName><FromUserName><![CDATA[${fromUser}]]></FromUserName><CreateTime>${Date.now()}</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[${content}]]></Content></xml>`);
 
 module.exports = {
   text,
