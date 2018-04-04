cb3b0bc4e0b1de55725b688fafc4b7d7b71c9430
Xu Haiming <haiming12322@qq.com>
2018-04-04 23:17:57 +0800

    Update response template

--- app/helpers/buildResponseTemplate.js
+++ app/helpers/buildResponseTemplate.js
@@ -1,23 +1,5 @@
 const text = ({ toUser, fromUser, content }) => (
-  `
-  <xml>
-    <ToUserName>
-      < ![CDATA[${toUser}] ]>
-    </ToUserName>
-    <FromUserName>
-      < ![CDATA[${fromUser}] ]>
-    </FromUserName>
-    <CreateTime>
-      ${Date.now()}
-    </CreateTime>
-    <MsgType>
-      < ![CDATA[text] ]>
-    </MsgType>
-    <Content>
-      < ![CDATA[${content}] ]>
-    </Content>
-  </xml>
-  `);
+  `<xml> <ToUserName>< ![CDATA[${toUser}] ]></ToUserName> <FromUserName>< ![CDATA[${fromUser}] ]></FromUserName> <CreateTime>${Date.now()}</CreateTime> <MsgType>< ![CDATA[text] ]></MsgType> <Content>< ![CDATA[${content}] ]></Content> </xml>`);
 
 module.exports = {
   text,
