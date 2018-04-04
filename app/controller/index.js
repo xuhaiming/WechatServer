47e2c9c751be977ab45ce5d350d2765dceb10f68
haimingxu <haimingxu@tencent.com>
2018-04-04 15:29:37 +0800

    Add post router

--- app/controller/index.js
+++ app/controller/index.js
@@ -4,8 +4,25 @@ class IndexController extends Controller {
   async gateway() {
     const { ctx } = this;
     const { query } = this.ctx;
+    const {
+      signature,
+      timestamp,
+      nonce,
+      echostr,
+    } = query;
 
+    console.log('---GET');
     console.log(query);
+
+    ctx.body = 'Hello world';
+  }
+
+  async gatewayPost() {
+    const { ctx } = this;
+    const { query } = this.ctx;
+
+    console.log('---POST');
+    console.log(ctx);
 
     ctx.body = 'Hello world';
   }
--- app/router.js
+++ app/router.js
@@ -1,4 +1,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/gateway', controller.index.gateway);
+  router.post('/gateway', controller.index.gatewayPost);
 };
